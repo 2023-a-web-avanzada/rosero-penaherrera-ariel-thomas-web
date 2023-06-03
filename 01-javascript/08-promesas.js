6e31e9e709b670f2231906453fcc63068083e28e
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-06-02 20:20:12 -0500

    Async y await

--- 01-javascript/08-promesas.js
+++ 01-javascript/08-promesas.js
@@ -1,34 +1,49 @@
 // 08-promesas.js
 const fs = require('fs');
-/* Una función que acepte como parametro una variable...*/
-function leerArchivo(path){
+/*
+* Una funcion que acepte como parametro una variable
+* del "path" del archivo y otra variable con el "contenidoArchivo".
+* Utilizar el modulo 'fs' para leer el archivo en ese "path" y anadir el
+* "contenidoArchivo" a ese archivo.
+* */
+function leerArchivo(path) {
     return new Promise(
-        (resolve,reject) => {
+        (resolve, reject) => {
             fs.readFile(
-                path, //Nombre o path del archivo
-                'utf-8', //Codificacion
+                path, // Nombre o path del archivo
+                'utf-8', // codificacion
                 (errorLectura, contenido) => {
-                    if(errorLectura) {
+                    if (errorLectura) {
                         console.error(errorLectura);
                         reject('Error leyendo primer archivo');
                     } else {
                         resolve(contenido)
                     }
                 }
-            )
+            );
         }
-    )
+    ); //
 }
 
-function escribirArchivo(path, contenido){
-    return new Promise((resolve, reject) => {
-        fs.writeFile(path, contenido, 'utf-8', (error) => {
-            if (error) {
-                console.log(error);
-            }
-        })
-    })
+function escribirArchivo(path, nuevoContenido) {
+    return new Promise(
+        (resolve, reject) => {
+            fs.writeFile(
+                path,
+                nuevoContenido,
+                (errorEscritura) => {
+                    if (errorEscritura) {
+                        console.error(errorEscritura);
+                        reject('Error escribiendo nuevo archivo');
+                    } else {
+                        resolve('Completado');
+                    }
+                }
+            );
+        }
+    ); //
 }
+
 
 function ejercicio(
     pathArchivoUno,
@@ -38,19 +53,45 @@ function ejercicio(
     let contenidoTotal = '';
     leerArchivo(pathArchivoUno)
         .then(
-            (contenidoPrimerArchivo) => {
+            (contenidoPrimerArchivo)=>{
                 contenidoTotal = contenidoTotal + contenidoPrimerArchivo;
-                return leerArchivo(pathArchivoDos)
+                return leerArchivo(pathArchivoDos);
             }
         )
         .then(
-            (contenidoSegundoArchivo) => {
-                contenidoTotal = contenidoTotal + contenidoSegundoArchivo;
-                return escribirArchivo(pathArchivoNuevo)
+            (contenidoSegundoArchivo)=>{
+                contenidoTotal = contenidoTotal + contenidoSegundoArchivo
+                return escribirArchivo(pathArchivoNuevo, contenidoTotal);
             }
         )
         .catch(error => console.error(error))
 }
 
-ejercicio('06-ejemplo.txt', '01-variables.js', '08-ejemploArchivo.txt')
+const pathUno = '01-variables.js';
+const pathDos = '06-ejemplo.txt';
+const pathTres = '06-ejemplo-respuesta.txt';
+ejercicio(pathUno, pathDos, pathTres);
 
+
+// ASYNC AWAIT
+// REGLAS:
+// 1) Estar dentro de una funcion (nombrada o anonima)
+// 2) AGREGAR la palabra 'async' antes de la declaracion
+//    de la funcion
+// 3) AGREGAR la palabra 'await' antes de la declaracion
+//    de una promesa
+// const a = async function(){}
+// const a = async ()=>{}
+async function ejercicioConAwait(){
+    const pathUno = '01-variables.js';
+    const pathDos = '06-ejemplo.txt';
+    const pathTres = '06-ejemplo-respuesta.txt';
+    try {
+        const contenidoUno = await leerArchivo(pathUno);
+        const contenidoDos = await leerArchivo(pathDos);
+        const contenidoTotal = contenidoUno + contenidoDos;
+        await escribirArchivo(pathTres, contenidoTotal);
+    } catch (error) {
+        console.error(error);
+    }
+}
