5c2b894c5b7f305cac89e70e2699289c853b4ddc
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-06-12 18:47:18 -0500

    React instalacion

--- 02-typescript/02-interfaces.ts
+++ 02-typescript/02-interfaces.ts
@@ -28,3 +28,27 @@ interface Usuario{
     //calcularImpuesto parametro numero impuesto, sueldo + sueldo * impuesto
     //estadoActual no reciba parametros, 'AP' 'AF' 'AT'
 }
+
+let user : Usuario = {
+    nombre: 'Adrian',
+    apellido: 'Eguezz',
+    casado: 0,
+    sueldo: 11.2,
+    estado: 'AC',
+    imprimirUsuario: (mensaje) => {
+        return 'El mensaje es: ' + mensaje;
+    },
+    calcularImpuesto: impuesto => {
+        return user.sueldo + user.sueldo * impuesto;
+    },
+    estadoActual: () => {
+        switch (user.estado) {
+            case 'AC':
+                return 'AP';
+            case 'IN':
+                return 'AF';
+            case 'BN':
+                return 'AT';
+        }
+    }
+}
