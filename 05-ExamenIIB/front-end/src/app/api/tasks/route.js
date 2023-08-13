e28097db740f9460af81bcdfd42b65d433793409
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-13 13:23:41 -0500

    ExamenIIB-Starting

---
+++ 05-ExamenIIB/front-end/src/app/api/tasks/route.js
@@ -0,0 +1,17 @@
+import {NextResponse} from "next/server";
+import {connectDB} from "@/utils/mongoose";
+
+export function GET() {
+    connectDB()
+
+
+    return NextResponse.json({
+        message: "Obteniendo tareas..."
+    });
+}
+
+export function POST(){
+    return NextResponse.json({
+        message: "Creando tarea...",
+    });
+}
