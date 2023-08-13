f3253a522ef1f6869a7e14ab8118b45e730c47f8
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-13 18:07:41 -0500

    FuncionGetDeUnaTarea ExamIIB

--- 05-ExamenIIB/front-end/src/app/api/tasks/[id]/route.js
+++ 05-ExamenIIB/front-end/src/app/api/tasks/[id]/route.js
@@ -1,10 +1,19 @@
 import {NextResponse} from "next/server";
+import {connectDB} from "@/utils/mongoose";
+import Task from '@/models/Task'
 
-export function GET(request, { params }){
-    console.log(params)
-    return NextResponse.json({
-        message: `Obteniendo tarea ${params.id}...`,
-    });
+export async function GET(request, { params }){
+    connectDB()
+    const taskFound = await Task.findById(params.id)
+
+    if (!taskFound)
+        return NextResponse.json({
+            message: "Task noty found",
+        },{
+            status: 404
+        })
+
+    return NextResponse.json(taskFound);
 }
 
 export function DELETE(request, {params}){
--- 05-ExamenIIB/front-end/src/app/api/tasks/route.js
+++ 05-ExamenIIB/front-end/src/app/api/tasks/route.js
@@ -1,17 +1,22 @@
 import {NextResponse} from "next/server";
 import {connectDB} from "@/utils/mongoose";
+import Task from '@/models/Task'
 
-export function GET() {
+export async function GET() {
     connectDB()
-
-
-    return NextResponse.json({
-        message: "Obteniendo tareas..."
-    });
+    const tasks = await Task.find()
+    return NextResponse.json(tasks);
 }
 
-export function POST(){
-    return NextResponse.json({
-        message: "Creando tarea...",
-    });
+export async function POST(request){
+    try {
+        const data =  await request.json() //Recibir info del cliente
+        const newTask = new Task(data)  //Crear un objeto de tarea
+        const savedTask = await newTask.save()  //Guarda en la BDD
+        return NextResponse.json(savedTask)
+    } catch (error) {
+        return NextResponse.json(error.message, {
+            status: 400
+        })
+    }
 }
