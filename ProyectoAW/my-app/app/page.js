80e1494b4a279463caf7afccd1759a472b481f20
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-16 10:59:22 -0500

    Empezar Proyecto
    VideoMin 8:30

---
+++ ProyectoAW/my-app/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <main className='py-5'>
+          <h1 className='text-xl md:text-5xl text-center font-bold py-10'>Employee Management</h1>
+      </main>
+    </>
+  )
+}
