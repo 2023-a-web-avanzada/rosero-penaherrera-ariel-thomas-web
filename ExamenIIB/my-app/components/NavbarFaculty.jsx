dfaa9417394200c0080c8a7b8af83ec8c05aa02d
Ariel Rosero <ariel.rosero@epn.edu.ec>
2023-08-25 14:39:28 -0500

    ExamenIIB Terminado Terminado :D

--- ExamenIIB/my-app/components/NavbarFaculty.jsx
+++ ExamenIIB/my-app/components/NavbarFaculty.jsx
@@ -1,16 +1,17 @@
+'use client'
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 
 export default function NavbarFaculty({params}){
 
-    //const router = useRouter();
-    //const {universityName} = router.query;
+    const searchUniversityName = useSearchParams()
+    const universityName = searchUniversityName.get('name')
 
     return(
         <>
             <nav className="flex justify-between items-center bg-green-800 px-8 py-3">
                 <Link className="text-white font-bold" href={`/faculties/${params}`}>Facultades</Link>
-                <p className="text-white font-bold">NombreUniversidad</p>
+                <p className="text-white font-bold">{universityName}</p>
                 <Link className="bg-white p-2" href={`/faculties/${params}/addFaculty`}>Crear Facultad</Link>
             </nav>
         </>
